bc8ee5dceead0831d8251c40c48c1884f8da9741
Justin Rayver L <justinrayver.14@gmail.com>
2026-01-12 10:58:20 +0800

    Add database connection script in db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; // Default XAMPP password is empty
+$dbname = "pao"; // Change this if your database name is different
+
+$conn = mysqli_connect("localhost","root","","pao");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
